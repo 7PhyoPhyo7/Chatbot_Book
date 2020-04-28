10464f443b1aa89b42710339a3470746182af488
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-04-29 01:05:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -357,7 +357,7 @@ requestify.post(sendmessageurl,
                                         })
 
                                        */
-                                       if(userInput == 'Start')
+                                       if(usermessage == 'Start')
                                        {
                                         textMessage(senderID,"Welcome User");
                                        }
